cc926a207f366222307fe74b30383bf39a18ccb7
oggydek <oggydek@users.noreply.github.com>
2026-06-11 17:47:05 +0530

    Added deploy configurations

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO-NAME>/',
+  base: '/VikingStat/',
   plugins: [react()],
-  server: 
+  server: {
+    port: 3000,
+  }
+})
